aaece36db230edd111df1adfe9df3081d8cb0bcb
Roy005 <150163834+Roy005@users.noreply.github.com>
2025-07-30 01:03:04 +0530

    Update Rect.java

--- Rect.java
+++ Rect.java
@@ -1,18 +1,4 @@
-<<<<<<< HEAD
-class Rect{
-  public static void main(String[] args) {
-   int l = Integer.parseInt(args[0]);
-   int w = Integer.parseInt(args[1]);
 
-   double area = (l * w);
-   double perimeter = 2 * (l + w);
-
-   System.out.println("The area is" + area);
-   System.out.println("The perimeter is" + perimeter);
-
-
-
-  }
 
   
 =======
